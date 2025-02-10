7690afeabced7852e5af058b8d2ebfcf44923c69
Vinit <vinitnagar56@gmail.com>
2025-02-10 17:19:21 +0530

    feat : RTCboard p2p live editor ✅

--- client/src/components/Docs.tsx
+++ client/src/components/Docs.tsx
@@ -1,30 +1,30 @@
 import React, { LegacyRef, useCallback, useEffect, useState } from 'react';
-import { Bold, List, Link2, AtSign, Code, X, Save } from 'lucide-react';
-import {  ToolbarButton } from '../types/types';
+import { Bold, List, Link2, AtSign, Code, Save, Trash } from 'lucide-react';
+import { ToolbarButton } from '../types/types';
 import { useRoomStore } from '../store/roomStore';
 
 function Docs() {
-    const { connection, docsElements , roomDetails } = useRoomStore()
+    const { connection, docsElements, roomDetails } = useRoomStore()
 
     const [selectedColor, setSelectedColor] = useState<string>('black');
 
     const toolbarButtons: ToolbarButton[] = [
-        {  tag: "strong", className : "font-bold", icon: <Bold size={16} /> },
-        {  tag: "h1", label: "H1", className: "text-3xl font-bold" },
-        {  tag: "h2", label: "H2", className: "text-2xl font-semibold" },
-        {  tag: "h3", label: "H3", className: "text-xl font-medium" },
-        {  tag: "p", label: "P", className: "text-base" },
+        { tag: "strong", className: "font-bold", icon: <Bold size={16} /> },
+        { tag: "h1", label: "H1", className: "text-3xl font-bold" },
+        { tag: "h2", label: "H2", className: "text-2xl font-semibold" },
+        { tag: "h3", label: "H3", className: "text-xl font-medium" },
+        { tag: "p", label: "P", className: "text-base" },
         { type: "divider" },
         { title: "•", tag: "ul", icon: <List size={16} />, className: "list-disc pl-5" },
         { type: "divider" },
-        { title: "example@gmail.com", tag: "a", icon: <Link2 size={16} />, className: "underline"},
+        { title: "example@gmail.com", tag: "a", icon: <Link2 size={16} />, className: "underline" },
         { title: "@", tag: "cite", icon: <AtSign size={16} />, className: "italic" },
         { tag: "code", icon: <Code size={16} />, className: "bg-gray-200 px-1 py-0.5 rounded text-sm font-mono" },
         { type: "spacer" },
     ];
 
     const addElement = (tag: keyof JSX.IntrinsicElements, title: string, className = "") => {
-        const newRef = React.createRef<HTMLElement>();  
+        const newRef = React.createRef<HTMLElement>();
         const newElement = { id: Date.now(), tag, text: title, className, ref: newRef as LegacyRef<HTMLInputElement>, color: selectedColor };
         useRoomStore.setState((state) => ({
             docsElements: { ...state.docsElements, elements: [...state.docsElements.elements, newElement] }
@@ -86,7 +86,7 @@ function Docs() {
 
     return (
         <div className="bg-gray-50 text-black">
-            <div className="max-w-4xl mx-auto">
+            <div className="w-full h-full">
                 <input
                     type="text"
                     value={docsElements.title || roomDetails?.roomName}
@@ -116,29 +116,29 @@ function Docs() {
                             <button className="p-2 rounded-full bg-gray-500 text-white selection:border-2 focus:border-4 border-gray-300" onClick={() => setSelectedColor('gray')}></button>
                             <button className="p-2 rounded-full bg-red-500 text-white focus:border-4 border-red-300" onClick={() => setSelectedColor('red')}></button>
                             <button className="p-2 rounded-full bg-black text-white focus:border-4 border-gray-700" onClick={() => setSelectedColor('black')}></button>
+                            <button className="p-2 rounded-full bg-blue-600 text-white focus:border-4 border-blue-400" onClick={() => setSelectedColor('blue')}></button>
                         </div>
                     </div>
 
                     <div className="px-4 py-3 h-[430px] overflow-y-scroll">
-                    {docsElements.elements.map((el) => (
-                    <div key={el.id} className="relative group flex items-center gap-2">
-                        <input
-                            type="text"
-                            ref={el.ref}
-                            value={el.text}
-                            onChange={(e) => updateElement(el.id, e.target.value)}
-                            className={`outline-none mb-1 p-1 w-full ${el.className}${
-                                el.tag === 'code' ? 'bg-gray-200 px-5 py-5 rounded text-sm font-mono' : ''}`}
-                            style={{ color: `${el.tag =='a' ?  'blue' : el?.color}` }}
-                        />
-                        <button
-                            className="text-red-500 opacity-0 group-hover:opacity-100 transition-opacity"
-                            onClick={() => removeElement(el.id)}
-                        >
-                            <X size={16} />
-                        </button>
-                    </div>
-))}
+                        {docsElements.elements.map((el) => (
+                            <div key={el.id} className="relative group flex items-center gap-2">
+                                <input
+                                    type="text"
+                                    ref={el.ref}
+                                    value={el.text}
+                                    onChange={(e) => updateElement(el.id, e.target.value)}
+                                    className={`outline-none mb-1 p-1 w-full ${el.className}${el.tag === 'code' ? 'bg-gray-200 px-5 py-5 rounded text-sm font-mono' : ''}`}
+                                    style={{ color: `${el.tag == 'a' ? 'blue' : el?.color}` }}
+                                />
+                                <button
+                                    className="text-red-500 opacity-0 group-hover:opacity-100 transition-opacity"
+                                    onClick={() => removeElement(el.id)}
+                                >
+                                    <Trash size={16} />
+                                </button>
+                            </div>
+                        ))}
 
                         <div
                             title='Save to populate changes on user2'
